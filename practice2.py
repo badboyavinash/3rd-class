8ceabc1385a3ba2fee3cec9cf52412991fa26fe4
badboyavinash <35717757+badboyavinash@users.noreply.github.com>
2018-02-06 10:22:32 +0530

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,23 @@
+#finding numbers using list
+"""n=10                   
+a=[1,2,3,4,5,6,7]
+b=[2,3,4,[1,2,n],9]
+print (b[2])
+print(b[3][2])"""
+
+
+#finding square root for a complex number
+"""import cmath
+import math
+b=cmath.sqrt(7)
+print (b)
+d=math.sqrt(7)
+print (int(d))"""
+
+#finding numbers using tuples
+"""a=(2,3,4,5,6,7)
+print (a[3])"""
+
+#using dictionary
+"""d={3:"Avinash" , "py":45}   #key :value
+print(d[3])"""
